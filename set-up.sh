257ef002ca59126e9bcca7ea8ed0fa41e3619938
Wesley Monte <wesley.monte@ccc.ufcg.edu.br>
2019-06-10 17:51:57 -0300

    Updated install docker

--- set-up.sh
+++ set-up.sh
@@ -2,24 +2,31 @@
 
 # Installing Docker
 
+apt update
+
 apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
     software-properties-common
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+CHECK_DOCKER_INSTALLATION=$(dpkg -l | grep -c docker-ce)
 
-apt-key fingerprint 0EBFCD88
+if ! [ $CHECK_DOCKER_INSTALLATION -ne 0 ]
+then
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+    sudo apt-key fingerprint 0EBFCD88
 
-apt-get update
+    sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
 
-apt-get install -y docker-ce
+    sudo apt-get update
+
+    sudo apt-get install -y docker-ce
+fi
 
 #Opening tcp port for api docker
 
