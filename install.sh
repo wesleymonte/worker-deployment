cc0de8134967578e29cc8d202af7a755fac258b3
Wesley Monte <wesley.monte@ccc.ufcg.edu.br>
2019-06-10 17:37:50 -0300

    Fixed host file and updated install.sh

--- install.sh
+++ install.sh
@@ -17,6 +17,10 @@ ANSIBLE_FILES_PATH=$(grep $ANSIBLE_FILES_PATH_PATTERN $HOSTS_CONF_FILE | awk -F 
 
 ANSIBLE_HOSTS_FILE=$ANSIBLE_FILES_PATH/"hosts"
 
+# Ansible ssh private key file path
+PRIVATE_KEY_FILE_PATH_PATTERN="ansible_ssh_private_key_file"
+PRIVATE_KEY_FILE_PATH=$(grep $PRIVATE_KEY_FILE_PATH_PATTERN $HOSTS_CONF_FILE | awk -F "=" '{print $2}')
+
 SIZE_PATTERN="size"
 SIZE=$(grep $SIZE_PATTERN $HOSTS_CONF_FILE | awk -F "=" '{print $2}')
 
@@ -26,6 +30,9 @@ DEPLOYED_WORKER_IP=$(grep $DEPLOYED_WORKER_IP_PATTERN $HOSTS_CONF_FILE | awk -F 
 sed -i 's/\[worker-machine\].*\[worker-machine:vars\]/[worker-machine]\n\n[worker-machine:vars]/' $ANSIBLE_HOSTS_FILE
 sed -i "2s/.*/$DEPLOYED_WORKER_IP/" $ANSIBLE_HOSTS_FILE
 
+# Writes the path of the private key file in Ansible hosts file
+sed -i "s#.*$PRIVATE_KEY_FILE_PATH_PATTERN=.*#$PRIVATE_KEY_FILE_PATH_PATTERN=$PRIVATE_KEY_FILE_PATH#" $ANSIBLE_HOSTS_FILE
+
 readonly DEPLOY_WORKER_YML_FILE="deploy-worker.yml"
 
-(cd $ANSIBLE_FILES_PATH && ansible-playbook -v $DEPLOY_WORKER_YML_FILE)
+(cd $ANSIBLE_FILES_PATH && ansible-playbook -vvv $DEPLOY_WORKER_YML_FILE)
--- set-up.sh
+++ set-up.sh
@@ -2,38 +2,38 @@
 
 # Installing Docker
 
-sudo apt-get install -y \
+apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
     software-properties-common
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
-sudo apt-key fingerprint 0EBFCD88
+apt-key fingerprint 0EBFCD88
 
-sudo add-apt-repository \
+add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 
-sudo apt-get update
+apt-get update
 
-sudo apt-get install -y docker-ce
+apt-get install -y docker-ce
 
 #Opening tcp port for api docker
 
-PROPERTY_PATTERN="ExecStart="
+PROPERTY_PATTERN="ExecStart"
 PROPERTY_VALUE="/usr/bin/dockerd -H fd:// -H=tcp://0.0.0.0:5555 --containerd=/run/containerd/containerd.sock"
 
 DOCKER_CONF_FILE="/lib/systemd/system/docker.service"
 
-sed -i "s@.*$PROPERTY_PATTERN = .*@$PROPERTY_PATTERN=$PROPERTY_VALUE@" $DOCKER_CONF_FILE
+sed -i "s@.*${PROPERTY_PATTERN}=.*@${PROPERTY_PATTERN}=${PROPERTY_VALUE}@" $DOCKER_CONF_FILE
 
 #Restarting docker service
 
-sudo systemctl daemon-reload
-sudo service docker restart
+systemctl daemon-reload
+service docker restart
 
-sudo docker pull wesleymonte/simple-worker
-sudo docker pull ubuntu:latest
+docker pull wesleymonte/simple-worker
+docker pull ubuntu:latest
