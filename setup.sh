af6d7df70ce0cac79f2f5e3eb1c898a62b44027e
Emanuel Joívo <emanueljoivo@gmail.com>
2019-06-12 08:27:57 -0300

    Removes sudo from apt installations

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# A script to setup a worker environment.
+# It expects to be run on Ubuntu 16.04 via 'sudo'
+
 install-docker() {
     echo "--> Installing docker"
-    sudo apt update
+    apt update
 
-    sudo apt-get install -y \
+    apt-get install -y \
         apt-transport-https \
         ca-certificates \
         curl \
@@ -12,16 +15,16 @@ install-docker() {
     
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-    sudo apt-key fingerprint 0EBFCD88
+    apt-key fingerprint 0EBFCD88
 
-    sudo add-apt-repository \
+    add-apt-repository \
     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
     $(lsb_release -cs) \
     stable"
 
-    sudo apt-get update
+    apt-get update
 
-    sudo apt-get install -y docker-ce
+    apt-get install -y docker-ce
 }
 
 open-tcp-port() {
