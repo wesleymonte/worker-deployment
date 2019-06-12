d686480f9a23578dc559000e677eec1eb2ed3e2f
Wesley Monte <wesley.monte@ccc.ufcg.edu.br>
2019-06-12 08:05:57 -0300

    Fixed sed to delete ips from hosts file

--- install.sh
+++ install.sh
@@ -23,24 +23,16 @@ PRIVATE_KEY_FILE_PATH=$(grep $PRIVATE_KEY_FILE_PATH_PATTERN $HOSTS_CONF_FILE | a
 
 
 DEPLOYED_WORKER_IP_PATTERN="deployed_worker_ip"
-#NUMBER_OF_WORKERS=$(grep $DEPLOYED_WORKER_IP $HOSTS_CONF_FILE | wc -l)
-sed -i 's/\[worker-machine\].*\[worker-machine:vars\]/\[worker-machine\]\n\n\[worker-machine:vars\]/' $ANSIBLE_HOSTS_FILE
+sed -i '/\[worker-machine\]/,/\[worker-machine:vars\]/{//!d}' $ANSIBLE_HOSTS_FILE
 
 grep $DEPLOYED_WORKER_IP_PATTERN $HOSTS_CONF_FILE| while read -r line ; do
     DEPLOYED_WORKER_IP=$(echo $line | awk -F "=" '{print $2}')
     sed -i "/\[worker-machine:vars\]/i ${DEPLOYED_WORKER_IP}" $ANSIBLE_HOSTS_FILE
 done
 
-# for counter in $(seq 1 ${NUMBER_OF_WORKERS})
-# do
-#   DEPLOYED_WORKER_IP_PATTERN="deployed_worker_ip_${counter}"
-  
-#   sed -i "/\[worker-machine:vars\]/i ${DEPLOYED_WORKER_IP}" $ANSIBLE_HOSTS_FILE
-# done
+# Writes the path of the private key file in Ansible hosts file
+sed -i "s#.*$PRIVATE_KEY_FILE_PATH_PATTERN=.*#$PRIVATE_KEY_FILE_PATH_PATTERN=$PRIVATE_KEY_FILE_PATH#" $ANSIBLE_HOSTS_FILE
 
-# # Writes the path of the private key file in Ansible hosts file
-# sed -i "s#.*$PRIVATE_KEY_FILE_PATH_PATTERN=.*#$PRIVATE_KEY_FILE_PATH_PATTERN=$PRIVATE_KEY_FILE_PATH#" $ANSIBLE_HOSTS_FILE
+readonly DEPLOY_WORKER_YML_FILE="deploy-worker.yml"
 
-# readonly DEPLOY_WORKER_YML_FILE="deploy-worker.yml"
-
-# (cd $ANSIBLE_FILES_PATH && ansible-playbook -vvv $DEPLOY_WORKER_YML_FILE)
+(cd $ANSIBLE_FILES_PATH && ansible-playbook -vvv $DEPLOY_WORKER_YML_FILE)
